182e504234cf5f4c7989eb6aef03a835fa744830
Yash Mishra <yash.mishra@raweng.com>
2019-06-14 12:19:22 +0530

    color app Part 1 completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Pallete from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div className="App">
-      <Pallete {...seedColors[5]} />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Pallete palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
