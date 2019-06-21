c257a8efbbc8411383e779d3b6695e34a5f835d8
Yash Mishra <yash.mishra@raweng.com>
2019-06-21 16:57:54 +0530

    Updates

---
+++ precache-manifest.43fa157ff067dc7f1f094bcafb8ff154.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedcc1ff1ff464ac0dc13089f0292c7c",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "6ca41a5402640b64334a",
+    "url": "/colors-app/static/css/2.c687f723.chunk.css"
+  },
+  {
+    "revision": "42bcde5a8bc4973cdabc",
+    "url": "/colors-app/static/css/main.bfb42a04.chunk.css"
+  },
+  {
+    "revision": "6ca41a5402640b64334a",
+    "url": "/colors-app/static/js/2.25a41b28.chunk.js"
+  },
+  {
+    "revision": "42bcde5a8bc4973cdabc",
+    "url": "/colors-app/static/js/main.5afab86f.chunk.js"
+  },
+  {
+    "revision": "658f8f8a04f3c7d8f86e",
+    "url": "/colors-app/static/js/runtime~main.7b7bc380.js"
+  },
+  {
+    "revision": "a411969677ce2c1dad7408d80976a396",
+    "url": "/colors-app/static/media/bg.a4119696.svg"
+  }
+]);
